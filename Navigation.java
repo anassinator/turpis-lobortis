54dbb7bdfe9ed6ff80aea82d2bb78620628e8682
Anass Al-Wohoush <anass.alwohoush@outlook.com>
2014-03-10 19:32:55 -0400

    second draft of navigation

--- Navigation.java
+++ Navigation.java
@@ -1,101 +1,98 @@
 /**
  * Navigation.java
- * @author: Mohamed Kleit, Anass Al-Wohoush
- * @version 0.1
+ * Navigates to opponent's flag, captures it and drops it off while avoiding obstacles
+ * @author Anass Al-Wohoush, Mohamed Kleit
+ * @version 0.2
  */
 
 import lejos.nxt.*;
 
 public class Navigation {
-	
-	public static Robot robot = new Robot; 
-	public static Odomoter odometer = new Odometer(robot);
-	public static Map map = new Map(odometer);
-	public static int[] courseInfo; 
-	
-	
+    public static Robot robot; 
+    public static Odometer odometer;
+    public static Map map;
+    public static int[] courseInfo; 
 
-		public Navigation(Robot robot, Odometer odometer,Map map, int[] courseInfo) {
-			
-			this.robot = robot;
-			this.odometer = odometer;
-			this.map = map;
-			this.courseInfo= courseInfo;
-	}
+    /**
+     * Navigation constructor
+     * @param robot object containing robot's dimensions and motor ports
+     * @param odometer object containing robot's position and orientation
+     * @param map object containing all obstacles
+     * @param course information containing opponent's flag location and drop off zone
+     */
+    public Navigation(Robot robot, Odometer odometer, Map map, int[] courseInfo) {
+        this.robot = robot;
+        this.odometer = odometer;
+        this.map = map;
+        this.courseInfo= courseInfo;
+    }
 
+    /** 
+     * Travels to coordinates x and y
+     * @param x coordinate in centimeters
+     * @param y coordinate in centimeters
+     */
+    public void travelTo(double x, double y) {
+        // ...
+    }
+    
+    /**
+     * Computes the minimal angle the robot should turn by
+     * according to the current theta read on the odometer
+     * @param degree in degrees
+     * @return the minimal angle in degrees
+     */
+    public static double getMinimalAngle(double degree) {    
+        // ...
+    } 
 
-		/** 
-		 * Travels to the point of coordinates x and y
-		 * @param x in cm
-		 * @param y in cm
-		 */
-		public void travelTo(double x, double y) {
+    /** 
+     * Sets motor speeds
+     * @param speed of left motor in degrees/second
+     * @param speed of right motor in degrees/second
+     */
+    public void setMotorSpeeds(double leftSpeed, double rightSpeed) {  
+        // ...
+    } 
+    
+    /**
+     * Sets rotate speed 
+     * @param speed of motor
+     */
+    public void setMotorRotateSpeed(double rotateSpeed) {   
+        setMotorSpeeds(rotateSpeed, -rotateSpeed); 
+    }
 
-		}
+    /** 
+     * Turns to the angle computed in getMinimalAngle method
+     * @param orientation in degrees
+     */
+    public void turnTo(double theta) {
+        // ...
+    }
 
-	
-		
-		/**
-		 * Computes the minimal angle the robot should turn by
-		 * according to the current theta read on the odometer
-		 * @param degree in degrees
-		 * @return the minimal angle in degrees
-		 */
-		public static double getMinimalAngle(double degree) {    
-			
-		} 
+    /**
+     * Stops the robot
+     */
+    public void stop() {   
+        // ...
+    }
 
-
-		
-		/** 
-		 * Sets motor speeds
-		 * @param leftSpeed in deg/sec
-		 * @param rightSpeed in deg/sec
-		 */
-		public void setMotorSpeeds(double leftSpeed, double rightSpeed) {  
-			
-		} 
-		
-		
-		/**
-		 * Sets rotate speed 
-		 * @param rotateSpeed
-		 */
-		public void setMotorRotateSpeed(double rotateSpeed) {   
-			setMotorSpeeds(rotateSpeed, -rotateSpeed); 
-		}
-
-
-		/** 
-		 * Turns to the angle computed in getMinimalAngle method
-		 * @param theta in degrees
-		 */
-		public void turnTo(double theta) {
-	
-		}
-
-		
-
-		/**
-		 * Stops the robot
-		 */
-		public void stop() {   
- 
-		}
-
-		
-		/**
-		 * Computes the angle each wheel should rotate 
-		 * in order to travel a certain distance
-		 * @param radius in cm
-		 * @param distance in cm
-		 * @return angle (in degrees) each wheel should rotate
-		 */
-		private static int convertDistance(double radius, double distance) {
-
-		}
-		
-		
-	}
-
-	
+    /**
+     * Avoids obstacle
+     */
+    public void avoid() {
+        // ...
+    }
+    
+    /**
+     * Computes the angle each wheel should rotate 
+     * in order to travel a certain distance
+     * @param radius in centimeters
+     * @param distance in centimeters
+     * @return angle (in degrees) each wheel should rotate
+     */
+    private static int convertDistance(double radius, double distance) {
+        // ...
+    }
+}
